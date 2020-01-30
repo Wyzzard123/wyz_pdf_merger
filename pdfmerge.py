aaa44682fa2c3a3fed3c0f669386c7ecf8bc75b8
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2020-01-30 16:51:23 +0800

    Some styling changes. Added second tab for merge_from_sequence, which allows you to merge from a sequence of files. However, it only allows you to merge files from the same folder without workarounds. Will implement a simple merge two pdfs function in the next commit.

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,12 +1,13 @@
-'''The functionality of the PDF Merger.'''
+"""The functionality of the PDF Merger."""
 
 import os
 
 import PyPDF2
 import re
 
+
 def check_inputs(*inputs):
-    '''Check that the input files exist. Otherwise, raise a FileNotFoundError.'''
+    """Check that the input files exist. Otherwise, raise a FileNotFoundError."""
     for input in inputs:
         if os.path.exists(input):
             continue
@@ -15,20 +16,24 @@ def check_inputs(*inputs):
     else:
         return True
 
+
 def check_folder(output_dir):
-    '''Check if an output_dir exists. Otherwise, create a new one.'''
+    """Check if an output_dir exists. Otherwise, create a new one."""
     if os.path.isdir(output_dir):
         return True
     else:
         os.makedirs(f"{output_dir}")
 
+
 def sanitize_input(input_file):
-    '''Get rid of \ in inputstring.'''
+    """Get rid of \\ in inputstring."""
     input_file = re.sub(r"\\", "/", input_file)
     return input_file
 
+
 def sanitize_file_path(output_dir, output_file_name):
-    '''Get rid of \, and make sure there are slashes between and .pdf at the end of the file name. Returns the sanitized file path.'''
+    """Get rid of \\, and make sure there are slashes between and .pdf at the end of the file name. Returns the
+    sanitized file path. """
 
     destination = ""
 
@@ -47,55 +52,58 @@ def sanitize_file_path(output_dir, output_file_name):
             destination = f"{output_dir}/{output_file_name}"
         else:
             destination = f"{output_dir}/{output_file_name}.pdf"
-    
+
     return destination
 
+
 def check_output_file_exists(destination):
-    '''If an output file already exists, raise an error.'''
+    """If an output file already exists, raise an error."""
     if os.path.exists(destination):
         raise FileExistsError(f"File already exists at {destination}. Change the output_file_name.")
-        
+
     else:
         return True
 
 
 def merge_two_pdfs(input1, input2, output_dir, output_file_name="mergedpdf"):
-    '''Simply merges two given PDFs together, with input1 first and input2 second. The PDF will be sent to the output directory (output_dir), which has a default name "mergedpdf.pdf".'''
-    
+    """Simply merges two given PDFs together, with input1 first and input2 second. The PDF will be sent to the output
+    directory (output_dir), which has a default name "mergedpdf.pdf". """
+
     input1 = sanitize_input(input1)
     input2 = sanitize_input(input2)
 
     # Check that the input file exists
     check_inputs(input1, input2)
 
-    
-
     # Create a File Merger.
     merger = PyPDF2.PdfFileMerger()
-    
+
     # Append both files to merger. import_bookmarks is set to False to avoid errors.
     merger.append(input1, import_bookmarks=False)
     merger.append(input2, import_bookmarks=False)
-    
-    # Check that the output folder exists, otherwise create a new folder. In merge_two_pdfs, this is done later than checking the inputs as there are only two inputs. We can then avoid creating a new directory where there is no need for one.
+
+    # Check that the output folder exists, otherwise create a new folder. In merge_two_pdfs, this is done later than
+    # checking the inputs as there are only two inputs. We can then avoid creating a new directory where there is no
+    # need for one.
 
     check_folder(output_dir)
 
     destination = sanitize_file_path(output_dir, output_file_name)
 
     check_output_file_exists(destination)
-    
+
     # Write to outfile
     with open(f'{destination}', 'wb') as outfile:
         merger.write(outfile)
     print(f"Output file to {destination}")
     return True
 
+
 def merge_many_pdfs(output_dir, output_file_name="mergedpdf", *inputs):
-    '''Merges PDFs according to their order in a list. The PDF will be sent to the output directory (output_dir), which has a default name "mergedpdf.pdf". 
-    
-    Make sure to input the file paths for the input and output folders as raw strings. Returns the destination and any errors.'''
-    
+    """Merges PDFs according to their order in a list. The PDF will be sent to the output directory (output_dir), which has a default name "mergedpdf.pdf".
+
+    Make sure to input the file paths for the input and output folders as raw strings. Returns the destination and any errors."""
+
     # Check that the output folder exists, otherwise create a new folder.
     check_folder(output_dir)
 
@@ -106,7 +114,8 @@ def merge_many_pdfs(output_dir, output_file_name="mergedpdf", *inputs):
     check_output_file_exists(destination)
     print(inputs)
     sanitized_inputs = []
-    # Check if the input files all exist. To facilitate this, first sanitize the names and add to sanitized inputs. Raise an error once a file does not exist.
+    # Check if the input files all exist. To facilitate this, first sanitize the names and add to sanitized inputs.
+    # Raise an error once a file does not exist.
 
     # TODO: Sanitize even when there is a backslash or when the string is not raw.
     for input in inputs:
@@ -115,9 +124,9 @@ def merge_many_pdfs(output_dir, output_file_name="mergedpdf", *inputs):
         sanitized_inputs.append(sanitized_input)
     # Create a File Merger.
     merger = PyPDF2.PdfFileMerger()
-    
+
     # Append all files to merger. import_bookmarks is set to False to avoid errors.
-    errors=[]
+    errors = []
     for sanitized_input in sanitized_inputs:
         try:
             print(sanitized_input)
@@ -126,17 +135,17 @@ def merge_many_pdfs(output_dir, output_file_name="mergedpdf", *inputs):
             print(f"OSError: The file {sanitized_input} might be corrupted.")
             errors.append(f"OSError: The file {sanitized_input} might be corrupted.")
 
-        
-    
     # Write to outfile
     with open(f'{destination}', 'ab') as outfile:
         merger.write(outfile)
     print(f"Output file to {destination}")
     return (destination, errors)
 
+
 def merge_pdfs_from_folder(input_dir, output_dir, output_file_name="mergedpdf"):
-    '''Merges many pdfs from one folder, and simply follows the order they are found in the folder. Return the destination folder and any errors encountered.'''
-    
+    """Merges many pdfs from one folder, and simply follows the order they are found in the folder. Return the
+    destination folder and any errors encountered. """
+
     # Regex filter at the end is to make sure only pdfs are merged.
     input_list = [r'{}/{}'.format(input_dir, f) for f in os.listdir(input_dir) if re.match(r"^.*\.pdf$", f)]
 
@@ -144,10 +153,8 @@ def merge_pdfs_from_folder(input_dir, output_dir, output_file_name="mergedpdf"):
     return (destination, errors)
 
 
-
-
 def main():
-    '''Test scripts.'''
+    """Test scripts."""
     test_output_dir = 'test_outputs'
     test_output_file = 'File2'
 
@@ -158,7 +165,7 @@ def main():
 
     # merge_many_pdfs(test_output_dir,test_output_file,*test_input_list)
     merge_pdfs_from_folder(test_input_dir, test_output_dir, "no.pdf")
-    
-    
+
+
 if __name__ == "__main__":
     main()
--- pdfmerge_main.py
+++ pdfmerge_main.py
@@ -6,25 +6,36 @@ from tkinter import ttk
 main = tk.Tk()
 main.title("Wyz's PDF Merger")
 main.iconbitmap('favicon.ico')
-main.geometry("300x200")
+main.geometry("300x300")
 
 
 def choose_folder_button(entry):
-    """Opens file dialog and fills out output_folder entry."""
-    folder = filedialog.askdirectory(initialdir="/", title="Select output folder")
+    """Opens file dialog and fills out output_folder or input_folder entry."""
+    folder = filedialog.askdirectory(initialdir="/", title="Select folder")
     if folder != "":
         entry.delete(0, tk.END)
         entry.insert(0, folder)
 
+def choose_file_button(entry):
+    """Opens file dialog and fills out input_file entry."""
+    folder = filedialog.askopenfilename(initialdir="/", title="Select files", filetypes=[("PDF file", "*.pdf")])
+    if folder != "":
+        entry.delete(0, tk.END)
+        entry.insert(0, folder)
 
-def folder_merge():
-    """GUI version of merge in pdf."""
+def choose_files_button(entry):
+    """Opens file dialog and fills out multiple file names."""
+    folder = filedialog.askopenfilenames(initialdir="/", title="Select file", filetypes=[("PDF file", "*.pdf")])
+    if folder != "":
+        entry.delete(0, tk.END)
+        entry.insert(0, folder)
+
+def folder_merge(output_folder, output_file, input_folder):
+    """GUI version of merge_pdfs_from_folder."""
     # Check for required input:
     if output_folder.get() == "":
         messagebox.showerror("Error: No Output Folder specified",
-                             '''Choose a folder where your new PDF will be located. Try not to copy paste directly 
-                             into this box if you are using Windows, as the backslashes may lead to issues. Use the 
-                             'Output' box instead.''')
+                             '''Choose a folder where your new PDF will be located. Try not to copy paste directly into this box if you are using Windows, as the backslashes may lead to issues. Use the 'Output' box instead.''')
         return
 
     if output_file.get() == "":
@@ -35,59 +46,149 @@ def folder_merge():
         return
 
     try:
-        destination, errors = pdfmerge.merge_pdfs_from_folder(input_folder.get(), output_folder.get(),
-                                                              output_file.get())
+        folder_merge_button.configure(state=tk.NORMAL)
+        destination, errors = pdfmerge.merge_pdfs_from_folder(input_folder.get(), output_folder.get(), output_file.get())
     except FileNotFoundError:
         messagebox.showerror("Error: File Not Found",
-                             f'''File {input_folder.get()} does not exist. Are there backslashes in the address? If 
-                             so, use the Output button instead of copy pasting a file address directly.''')
+                             f'''File {input_folder.get()} does not exist. Are there backslashes in the address? If so, use the Input or Output button instead of copy pasting a file address directly.''')
         return
     except FileExistsError:
         messagebox.showerror("Error: File Already Exists",
                              f"File already exists. Change the output file name to something else.")
         return
+    else:
+        if not errors:
+            messagebox.showinfo("Complete", f"Output file to {destination}.")
+        elif errors:
+            # error_string = len(errors) * "{:error}\n".format(*errors)
 
-    if not errors:
-        messagebox.showinfo("Complete", f"Output file to {destination}.")
-    elif errors:
-        # error_string = len(errors) * "{:error}\n".format(*errors)
+            messagebox.showinfo("Complete",
+                                f"Output file to {destination}.\n\nYou encountered the following errors:\n\n{errors}")
+    finally:
+        folder_merge_button.configure(state=tk.NORMAL)
 
-        messagebox.showinfo("Complete",
-                            f"Output file to {destination}.\n\nYou encountered the following errors:\n\n{errors}")
+def sequence_merge(output_folder, output_file, input_files):
+    """GUI version of merge_many_pdfs."""
+    # Check for required input:
+    
+    
+    if output_folder.get() == "":
+        messagebox.showerror("Error: No Output Folder specified",
+                             '''Choose a folder where your new PDF will be located. Try not to copy paste directly into this box if you are using Windows, as the backslashes may lead to issues. Use the 'Output' box instead.''')
+        return
 
-    merge_from_folder_button.configure(state=tk.NORMAL)
+    if output_file.get() == "":
+        messagebox.showerror("Error: No Output File specified", "Type a filename for your merged pdf.")
+        return
+    if input_files.get() == "":
+        messagebox.showerror("Error: No Input Files specified", "Choose PDfs to merge.")
+        return
 
+    try:
+        input_files_list = main.tk.splitlist(input_files.get())
+        print(input_files_list)
+        destination, errors = pdfmerge.merge_many_pdfs(output_folder.get(), output_file.get(), *input_files_list)
+        sequence_merge_button.configure(state=tk.DISABLED)
+    except FileNotFoundError:
+        messagebox.showerror("Error: File Not Found",
+                             f'''Are there backslashes in any address? If so, use the Input button instead of copy pasting a file address directly.''')
+    except FileExistsError:
+        messagebox.showerror("Error: File Already Exists",
+                             f"File already exists. Change the output file name to something else.")
+    else:
+        if not errors:
+            messagebox.showinfo("Complete", f"Output file to {destination}.")
+        elif errors:
+            # error_string = len(errors) * "{:error}\n".format(*errors)
+
+            messagebox.showinfo("Complete",
+                                f"Output file to {destination}.\n\nYou encountered the following errors:\n\n{errors}")
+    finally:
+        sequence_merge_button.configure(state=tk.NORMAL)
+
+    
+
+# Create a notebook
+nb = ttk.Notebook(main)
+nb.grid(row=0, column=0, columnspan=50, sticky='NESW')
+nb.rowconfigure(0, weight=1)
+nb.columnconfigure(0, weight=1)
+
+# TAB 1: Merge All PDFs in Folder #####################################
+# "f" added to the end of variables here to represent folder.
+folder_merge_tab = ttk.Frame(nb)
+nb.add(folder_merge_tab, text='Merge PDFs in Folder')
 
 # Output folder
-output_folder_label = ttk.Label(text="Choose Output Folder:")
-output_folder_label.grid(row=0, column=0)
-output_folder = ttk.Entry()
-output_folder.grid(row=1, column=0)
+output_folder_label_f = ttk.Label(folder_merge_tab, text="Choose Output Folder:")
+output_folder_label_f.grid(row=0, column=0)
+output_folder_f = ttk.Entry(folder_merge_tab)
+output_folder_f.grid(row=1, column=0)
 
 # Output Button
-output_button = ttk.Button(text="Output", command=lambda: choose_folder_button(output_folder))
-output_button.grid(row=1, column=1)
+output_button_f = ttk.Button(folder_merge_tab, text="Output", command=lambda: choose_folder_button(output_folder_f))
+output_button_f.grid(row=1, column=1)
 
 # Output File
-output_file_label = ttk.Label(text="Type Output File Name:")
-output_file_label.grid(row=2, column=0)
-output_file = ttk.Entry()
-output_file.grid(row=3, column=0)
+output_file_label_f = ttk.Label(folder_merge_tab, text="Type Output File Name:")
+output_file_label_f.grid(row=2, column=0)
+output_file_f = ttk.Entry(folder_merge_tab)
+output_file_f.grid(row=3, column=0)
 
 # Input folder
-input_folder_label = ttk.Label(text="Choose Input Folder:")
-input_folder_label.grid(row=4, column=0)
-input_folder = ttk.Entry()
-input_folder.grid(row=5, column=0)
+input_folder_label_f = ttk.Label(folder_merge_tab, text="Choose Input Folder:")
+input_folder_label_f.grid(row=4, column=0)
+input_folder_f = ttk.Entry(folder_merge_tab)
+input_folder_f.grid(row=5, column=0)
 
 # Input Button
-input_button = ttk.Button(text="Input", command=lambda: choose_folder_button(input_folder))
-input_button.grid(row=5, column=1)
+input_button_f = ttk.Button(folder_merge_tab, text="Input", command=lambda: choose_folder_button(input_folder_f))
+input_button_f.grid(row=5, column=1)
 
 # Merge all in folder
-merge_from_folder_button = ttk.Button(text="Merge from Input Folder", command=folder_merge)
-merge_from_folder_button.grid(row=6, column=0)
+folder_merge_button = ttk.Button(folder_merge_tab, text="Merge from Input Folder",
+                                      command=lambda: folder_merge(output_folder_f, output_file_f, input_folder_f))
+folder_merge_button.grid(row=6, column=0, padx=(10, 0), pady=10)
 
-button = tk.Button()
+# TAB 2: Merge All PDFs in Sequence #####################################
+# "s" added to the end of variables here to represent sequence.
+seq_merge_tab = ttk.Frame(nb)
+nb.add(seq_merge_tab, text='Merge PDFs in Sequence')
+
+# Output folder
+output_folder_label_s = ttk.Label(seq_merge_tab, text="Choose Output Folder:")
+output_folder_label_s.grid(row=0, column=0)
+output_folder_s = ttk.Entry(seq_merge_tab)
+output_folder_s.grid(row=1, column=0)
+
+# Output Button
+output_button_s = ttk.Button(seq_merge_tab, text="Output", command=lambda: choose_folder_button(output_folder_s))
+output_button_s.grid(row=1, column=1)
+
+# Output File
+output_file_label_s = ttk.Label(seq_merge_tab, text="Type Output File Name:")
+output_file_label_s.grid(row=2, column=0)
+output_file_s = ttk.Entry(seq_merge_tab)
+output_file_s.grid(row=3, column=0)
+
+# Input files
+input_files_label_s = ttk.Label(seq_merge_tab, text="Choose Input File(s):")
+input_files_label_s.grid(row=4, column=0)
+input_files_s = ttk.Entry(seq_merge_tab)
+input_files_s.grid(row=5, column=0)
+
+# Input Button
+input_button_s = ttk.Button(seq_merge_tab, text="Input", command=lambda: choose_files_button(input_files_s))
+input_button_s.grid(row=5, column=1)
+
+# Merge all in sequence
+sequence_merge_button = ttk.Button(seq_merge_tab, text="Merge from Input Files", command=lambda: sequence_merge(output_folder_s, output_file_s, input_files_s))
+sequence_merge_button.grid(row=6, column=0, padx=(10, 0), pady=10)
+
+
+
+
+
+# MAIN LOOP
 
 tk.mainloop()
